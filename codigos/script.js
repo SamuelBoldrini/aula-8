588da56b456dbd60f5221c94de7ac0d1cfb21dec
SamuelBoldrini <samuelboldrinivelten@gmail.com>
2024-08-02 16:01:44 -0300

    imcCalc

--- codigos/script.js
+++ codigos/script.js
@@ -1,7 +1,11 @@
-// Busque os elementos HTML
+
 
 formulario.onsubmit = (event) => {
   event.preventDefault(); // Impede o comportamento padrão de envio do formulário
-
-  // Sua lógica aqui!
+  const weight = document.getElementById("weight").value
+  const height = document.getElementById("height").value
+  let imcValue = Math.round(weight * 10 / (height * height))/10
+  document.getElementById("imcResult").textContent = `Seu IMC é ${imcValue}`
 };
+
+document.getElementById("calcButton").onclick = onsubmit
