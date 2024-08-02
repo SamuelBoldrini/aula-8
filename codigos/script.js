7723bc264732cc1eaede4b11436247ccb015cb57
wallacevieira <wallaceofc@hotmail.com>
2024-08-02 07:40:30 -0300

    feat: para o desafio

--- codigos/script.js
+++ codigos/script.js
@@ -1,14 +1,7 @@
-const botao = document.querySelector("button");
-const resultadoTexto = document.querySelector("#resultadoTexto");
+// Busque os elementos HTML
 
-function calculoImc(altura, peso) {
-  return peso / (altura * altura);
-}
+formulario.onsubmit = (event) => {
+  event.preventDefault(); // Impede o comportamento padrão de envio do formulário
 
-botao.onclick = () => {
-  const altura = parseFloat(document.querySelector("#altura").value);
-  const peso = parseFloat(document.querySelector("#peso").value);
-
-  const imc = calculoImc(altura, peso);
-  resultadoTexto.textContent = `Seu IMC é ${imc.toFixed(2)}`;
+  // Sua lógica aqui!
 };
